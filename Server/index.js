7a862964f44e82720db3f5ea29fbc9b898c29859
The Blackened Soul <113580160+sohagannamA@users.noreply.github.com>
2024-11-23 14:36:01 +0800

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -13,7 +13,6 @@ const fs = require("fs");
 // app.use("/Image",express.static(path.join(__dirname,"/Image")));
 app.use("/Image", express.static(path.join(__dirname, "/Image")));
 
-const port = 3000;
 const host = "127.0.0.1";
 const port = process.env.PORT || 4000
 const dataConect = async () => {
